dc859b411b5a08eb886783d1ae70b3861e6275cc
Pedroregueira <pedromion20@gmail.com>
2025-12-17 16:03:18 -0300

    apagar_linhas.py

--- apagar_linhas.py
+++ apagar_linhas.py
@@ -1,17 +1,31 @@
+import streamlit as st
 from openpyxl import load_workbook
+import os
 
-def apagar_linhas_1_a_5(arquivo_entrada, arquivo_saida):
-    wb = load_workbook(arquivo_entrada)
+st.set_page_config(page_title="Teste - Apagar Linhas", layout="centered")
+st.title("Apagar linhas 1 a 5")
+
+def apagar_linhas_1_a_5(uploaded_file, arquivo_saida):
+    wb = load_workbook(uploaded_file)
     ws = wb.worksheets[0]  # primeira aba
 
-    # Apaga linhas 1 a 5
+    # Apaga fisicamente as linhas 1 a 5
     ws.delete_rows(1, 5)
 
     wb.save(arquivo_saida)
 
+arquivo = st.file_uploader("Envie o Excel", type=["xlsx"])
 
-# EXEMPLO DE USO
-apagar_linhas_1_a_5(
-    "entrada.xlsx",
-    "saida_sem_linhas_1_a_5.xlsx"
-)
+if arquivo:
+    saida = "sem_linhas_1_a_5.xlsx"
+
+    apagar_linhas_1_a_5(arquivo, saida)
+
+    with open(saida, "rb") as f:
+        st.download_button(
+            "Baixar arquivo",
+            f,
+            file_name="sem_linhas_1_a_5.xlsx"
+        )
+
+    os.remove(saida)
