55d02efad558915c17ed71393bbf78be37ea9c79
Pedroregueira <pedromion20@gmail.com>
2025-12-17 16:00:41 -0300

    Add files via upload

--- apagar_linhas.py
+++ apagar_linhas.py
@@ -1,1 +1,17 @@
-apagar_linhas.py
+from openpyxl import load_workbook
+
+def apagar_linhas_1_a_5(arquivo_entrada, arquivo_saida):
+    wb = load_workbook(arquivo_entrada)
+    ws = wb.worksheets[0]  # primeira aba
+
+    # Apaga linhas 1 a 5
+    ws.delete_rows(1, 5)
+
+    wb.save(arquivo_saida)
+
+
+# EXEMPLO DE USO
+apagar_linhas_1_a_5(
+    "entrada.xlsx",
+    "saida_sem_linhas_1_a_5.xlsx"
+)
